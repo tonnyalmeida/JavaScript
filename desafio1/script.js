03aee8cb134cb9bbea978bee8e4a431013dc56db
TonnyAlmeidaa <166747870+TonnyAlmeidaa@users.noreply.github.com>
2024-04-30 01:33:44 -0300

    mudanças

--- desafio1/script.js
+++ desafio1/script.js
@@ -2,23 +2,28 @@ function carregar() {
     var msg = window.document.getElementById('msg')
     var img = window.document.getElementById('foto')
     var data = new Date()
-    var hora = data.getHours()
+    //var hora = data.getHours()
+    var hora = 5
     msg.innerHTML = `Agora são ${hora} horas.`
-    if (hora >= 0 && hora < 12) {
+    if (hora >= 0 && hora < 6) {
         //bom dia
+        img.style.backgroundImage = "url('imagens/foto-madrugada.jpg')"
+        document.body.style.background = '#5D559A'
+    } else if (hora >= 6 && hora <= 11) {
+        //boa tarde
         img.style.backgroundImage = "url('imagens/foto-manha.jpg')"
         document.body.style.background = '#FCC06B'
     } else if (hora >= 12 && hora < 18) {
-        //boa tarde
+        //boa noite
         img.style.backgroundImage = "url('imagens/foto-tarde.jpg')"
         document.body.style.background = '#9AD1DA'
     } else {
-        //boa noite
         img.style.backgroundImage = "url('imagens/foto-noite.jpg')"
-        document.body.style.background = '#2D3F4D'
+        document.body.style.background = '#023165'
     }
 }
 
 // cor manha : #FCC06B
 //cor tarde : #9AD1DA
-//cor noite : #101418
+//cor noite : #023165
+//cor da madruga : #5D559A
