5ca1e2b5df219a943981c0c474b7cfc93b1f7cab
TonnyAlmeidaa <166747870+TonnyAlmeidaa@users.noreply.github.com>
2024-04-30 01:54:23 -0300

    pequenos ajustes

--- desafio01/script.js
+++ desafio01/script.js
@@ -2,18 +2,17 @@ function carregar() {
     var msg = window.document.getElementById('msg')
     var img = window.document.getElementById('foto')
     var data = new Date()
-    //var hora = data.getHours()
-    var hora = 5
+    var hora = data.getHours()
     msg.innerHTML = `Agora são ${hora} horas.`
     if (hora >= 0 && hora < 6) {
         //bom dia
         img.style.backgroundImage = "url('imagens/foto-madrugada.jpg')"
         document.body.style.background = '#5D559A'
-    } else if (hora >= 6 && hora <= 11) {
+    } else if (hora <= 11) {
         //boa tarde
         img.style.backgroundImage = "url('imagens/foto-manha.jpg')"
         document.body.style.background = '#FCC06B'
-    } else if (hora >= 12 && hora < 18) {
+    } else if (hora < 18) {
         //boa noite
         img.style.backgroundImage = "url('imagens/foto-tarde.jpg')"
         document.body.style.background = '#9AD1DA'
